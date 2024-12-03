6206662ba4bedf38c715117637f2cc63696a12ec
EmilioD0608 <emiliodelgadoladines2014@gmail.com>
2024-12-03 09:22:13 -0500

    creado htmls bases

--- Aplicaciones/Tienda_Virtual/apps.py
+++ Aplicaciones/Tienda_Virtual/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TiendaVirtualConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Tienda_Virtual'
+    name = 'Aplicaciones.Tienda_Virtual'
