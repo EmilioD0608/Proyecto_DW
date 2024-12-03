99f23cf939d2f1b767b7ba51491837033b6b8880
Adrius <eladriaxoiezi17@gmail.com>
2024-12-03 14:46:48 -0500

    Adrián

--- Aplicaciones/Tienda_Virtual/urls.py
+++ Aplicaciones/Tienda_Virtual/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.inicio, name='Inicio')
+    path('', views.inicio, name='Inicio'),
+    path('ofertas/', views.ofertas, name='ofertas'),
+    path('computadoras/', views.computadoras, name='computadoras'),
+    path('laptops/', views.laptops, name='laptops'),
+    path('teclados/', views.teclados, name='teclados'),
+    path('mouse/', views.mouse, name='mouse'),
 ]
--- Aplicaciones/Tienda_Virtual/views.py
+++ Aplicaciones/Tienda_Virtual/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
+def ofertas(request):
+    return render(request, 'ofertas.html')
+
+def computadoras(request):
+    return render(request, 'computadoras.html')
+
+def laptops(request):
+    return render(request, 'laptops.html')
+
+def teclados(request):
+    return render(request, 'teclados.html')
+
+def mouse(request):
+    return render(request, 'Mouses.html')
